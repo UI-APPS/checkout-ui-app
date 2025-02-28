97618a16eb202f200fa11c836354078e77f7ccce
Marketplace Wromo Studio <info.wromo@gmail.com>
2025-02-28 22:22:06 +0100

    add

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,7 @@ const nextConfig: NextConfig = {
         STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     },
     // Optional: Configure API routes to be server-side only
-    output: 'standalone',
-    experimental: {
-      serverComponentsExternalPackages: ['@cloudflare/next-on-pages'],
-    },
+    
   }
   
   export default nextConfig
